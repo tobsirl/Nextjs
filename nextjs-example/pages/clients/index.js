0212f8b4e1451e38327dcce929e4144a0595349b
Paul Tobin <tobster111@gmail.com>
2021-03-06 14:55:13 +0000

    Dynamically creating links

--- nextjs-example/pages/clients/index.js
+++ nextjs-example/pages/clients/index.js
@@ -1,8 +1,20 @@
+import Link from 'next/link';
 export default function ClientsPage() {
- 
+  const clients = [
+    { id: '001', name: 'Paul' },
+    { id: '002', name: 'Keith' },
+    { id: '003', name: 'Simon' },
+  ];
+
+  const data = clients.map(({ id, name }) => (
+    <li key={id}>
+      <Link href={`/clients/${id}`}>{name}</Link>
+    </li>
+  ));
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>{data}</ul>
     </div>
   );
 }
