359a93268984571a4881aedbfc58a5123cf967ba
Paul Tobin <tobster111@gmail.com>
2021-03-06 15:03:04 +0000

    Navigating Programmtically

--- nextjs-example/pages/clients/[id]/index.js
+++ nextjs-example/pages/clients/[id]/index.js
@@ -2,10 +2,17 @@ import { useRouter } from 'next/router';
 
 export default function ClientProjectsPage() {
   const router = useRouter();
+
+  function loadProjectHandler() {
+    // load data
+    router.push('/clients/001/projecta');
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
       <h3>{router.query.id}</h3>
+      <button onClick={loadProjectHandler}>Load Project</button>
     </div>
   );
 }
