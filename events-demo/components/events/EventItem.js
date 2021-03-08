17e9dfdd4fc5a7307c1798d0ba01973041c8531e
Paul Tobin <tobster111@gmail.com>
2021-03-08 18:31:46 +0000

    Adding styling to the app

--- events-demo/components/events/EventItem.js
+++ events-demo/components/events/EventItem.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import classes from './EventItem.module.css';
+
 export default function EventItem({ title, date, image, location, id }) {
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
     day: 'numeric',
@@ -12,19 +14,19 @@ export default function EventItem({ title, date, image, location, id }) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- events-demo/components/events/EventList.js
+++ events-demo/components/events/EventList.js
@@ -1,10 +1,11 @@
 import EventItem from './EventItem';
 
+import classes from './EventList.module.css'
 export default function EventList(props) {
   const { events } = props;
   // console.log(events);
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map((event) => (
         <EventItem key={event.id} {...event} />
       ))}
