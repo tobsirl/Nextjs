8fcef0c1334246da9320df832383ff266556ee01
Paul Tobin <tobster111@gmail.com>
2021-03-08 18:49:20 +0000

    Adding icons

--- events-demo/components/events/EventItem.js
+++ events-demo/components/events/EventItem.js
@@ -1,3 +1,6 @@
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 import Button from '../ui/Button';
 import classes from './EventItem.module.css';
 
@@ -19,14 +22,21 @@ export default function EventItem({ title, date, image, location, id }) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
