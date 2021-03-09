c43e67288d653df7e4bbe78ae7290723d463918e
Paul Tobin <tobster111@gmail.com>
2021-03-09 18:39:52 +0000

    Using the event-details components

--- events-demo/pages/events/[eventsId].js
+++ events-demo/pages/events/[eventsId].js
@@ -1,16 +1,30 @@
 import { useRouter } from 'next/router';
 import { getEventById } from '../../dummy-data';
+import React from 'react';
+
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 export default function EventsDetailPage() {
   const { query } = useRouter();
   const event = getEventById(query.eventsId);
-  
-  if(!event) {
-    return <p>No event found!</p>
+
+  if (!event) {
+    return <p>No event found!</p>;
   }
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-      <h3>Show Selected Event {query.eventsId}</h3>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
