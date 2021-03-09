edf61b91ef4d95ba2392e19584e63ab29f137cce
Paul Tobin <tobster111@gmail.com>
2021-03-09 18:32:48 +0000

    Using useRouter to get the page id, using that to get the dummy data

--- events-demo/pages/events/[eventsId].js
+++ events-demo/pages/events/[eventsId].js
@@ -1,10 +1,16 @@
 import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
 export default function EventsDetailPage() {
-  const router = useRouter();
+  const { query } = useRouter();
+  const event = getEventById(query.eventsId);
+  
+  if(!event) {
+    return <p>No event found!</p>
+  }
   return (
     <div>
       <h1>Event Detail Page</h1>
-      <h3>Show Selected Event {router.query.eventsId}</h3>
+      <h3>Show Selected Event {query.eventsId}</h3>
     </div>
   );
 }
