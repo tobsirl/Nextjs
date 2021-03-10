f250fdd06ee037b2146427638f2071d941156c53
Paul Tobin <tobster111@gmail.com>
2021-03-10 19:25:49 +0000

    Adding dropdown menu for months

--- events-demo/components/events/EventsSearch.js
+++ events-demo/components/events/EventsSearch.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import Button from '../../components/ui/Button';
 
 export default function EventsSearch(props) {
   return (
@@ -11,7 +11,25 @@ export default function EventsSearch(props) {
             <option value="2022">2022</option>
           </select>
         </div>
+        <div>
+          <label htmlFor="month">Month</label>
+          <select name="" id="month">
+            <option value="1">January</option>
+            <option value="2">Feburary</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+        </div>
       </div>
+      <Button>Find Events</Button>
     </form>
   );
 }
