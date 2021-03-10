f4733057cdef61ed22bf7ca7d5ea24ad71d1cf93
Paul Tobin <tobster111@gmail.com>
2021-03-10 19:55:47 +0000

    Using useRouter to programmically change the page based on year/month

--- events-demo/pages/events/index.js
+++ events-demo/pages/events/index.js
@@ -1,11 +1,15 @@
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/EventList';
 import EventsSearch from '../../components/events/EventsSearch';
+import { useRouter } from 'next/router';
 
 export default function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
 
-  function findEventsHandler(year, month) {}
+  function findEventsHandler(year, month) {
+    router.push(`/events/${year}/${month}`);
+  }
 
   return (
     <>
