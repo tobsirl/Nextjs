bcd617861e55c7d4fa63b166d98c09652422b8f5
Paul Tobin <tobster111@gmail.com>
2021-03-11 17:57:32 +0000

    Filtering events

--- events-demo/pages/events/[...slug].js
+++ events-demo/pages/events/[...slug].js
@@ -1,4 +1,22 @@
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
+
 export default function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) return <p className="center">Loading...</p>;
+
+  const [year, month] = filterData;
+
+  const numYear = parseInt(year);
+  const numMonth = parseInt(month);
+  
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  console.log(filteredEvents);
+
   return (
     <div>
       <h1>Filtered Events</h1>
