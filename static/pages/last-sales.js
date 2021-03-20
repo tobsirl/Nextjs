8ed131add98116937974cb36ca8c4d7095375251
Paul Tobin <tobster111@gmail.com>
2021-03-20 11:34:26 +0000

    Using a combination of server side and client side rendering

--- static/pages/last-sales.js
+++ static/pages/last-sales.js
@@ -3,20 +3,19 @@ import useSWR from 'swr';
 import swr from 'swr';
 
 export default function LastSales({ dataServer }) {
+  const [todos, setTodos] = useState(dataServer);
   console.log('dataServer :>> ', dataServer);
-  const { data, error } = useSWR(
-    'https://jsonplaceholder.typicode.com/todos/'
-  );
+  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/todos/');
 
   if (error) return <div>Failed to load...</div>;
 
-  if (!data) return <div>Loading</div>;
+  if (!data && !todos) return <div>Loading</div>;
 
   console.log('data :>> ', data);
 
   return (
     <ul>
-      {data.map((todo) => (
+      {todos.map((todo) => (
         <li key={todo.id}>{todo.title}</li>
       ))}
     </ul>
