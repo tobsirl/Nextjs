41256e2b04b6e8f3e36bc2060efa32f9046706fc
Paul Tobin <tobster111@gmail.com>
2021-03-20 11:07:22 +0000

    Using SWR

--- static/pages/last-sales.js
+++ static/pages/last-sales.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import useSWR from 'swr';
+import swr from 'swr';
 
 export default function LastSales() {
-  const [todos, setTodos] = useState([]);
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/')
-      .then((response) => response.json())
-      .then((data) => {
-        setTodos(data);
-        console.log(data);
-      });
-  }, []);
+  const [todos, setTodos] = useState();
+
+  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/todos/');
+
+  if (error) return <div>Failed to load...</div>;
+
+  if (!data) return <div>Loading</div>;
+
   return (
     <ul>
-      {todos.map((todo) => (
+      {data.map((todo) => (
         <li key={todo.id}>{todo.title}</li>
       ))}
     </ul>
