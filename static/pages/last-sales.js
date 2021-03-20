36fc7a5e84241ca1068bea7dd85ed05886fe5a31
Paul Tobin <tobster111@gmail.com>
2021-03-20 11:21:11 +0000

    Using getStaticProps to pre fetch on the server

--- static/pages/last-sales.js
+++ static/pages/last-sales.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from 'react';
 import useSWR from 'swr';
 import swr from 'swr';
 
-export default function LastSales() {
-  const [todos, setTodos] = useState();
-
-  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/todos/');
+export default function LastSales({ dataServer }) {
+  console.log('dataServer :>> ', dataServer);
+  const { data, error } = useSWR(
+    'https://jsonplaceholder.typicode.com/todos/'
+  );
 
   if (error) return <div>Failed to load...</div>;
 
   if (!data) return <div>Loading</div>;
+
+  console.log('data :>> ', data);
 
   return (
     <ul>
@@ -18,4 +21,14 @@ export default function LastSales() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+
+  const dataServer = await response.json();
+
+  return {
+    props: { dataServer },
+  };
 }
